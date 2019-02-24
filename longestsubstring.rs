134994636ed11d4089b0c6bd730030f720f8a51a
Michael Droessler <droessmj@gmail.com>
2019-02-24 16:38:04 -0600

    speedup attempt 1

--- longestsubstring.rs
+++ longestsubstring.rs
@@ -2,12 +2,15 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let (mut max, mut count, mut i) = (0,0,0);
+        let mut max:i32 = 0;
+        let mut count:i32 = 0;
+        let mut i:i32 = 0;
+        let len:i32 = s.len() as i32;
         let mut map = HashMap::new();
         let chars: Vec<char> = s.chars().collect();
         
-        while i < s.len(){
-            let c: char = chars[i];
+        while i < len && ((count + (len - i)) > max){
+            let c: char = chars[i as usize];
             if !map.contains_key(&c){
                 count += 1;
                 map.insert(c,i);
@@ -17,7 +20,7 @@ impl Solution {
                 if let Some(j) = map.get(&c) {
                     i = *j + 1;              
                 }
-                map = HashMap::new();
+                map.clear();
             }
             if count > max {
                 max = count;
