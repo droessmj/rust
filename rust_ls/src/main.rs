d28d137bfb8df6f5d4f5d70e758ced448d78a12b
Michael Droessler <droessmj@gmail.com>
2019-03-10 15:22:07 -0500

    working version

--- rust_ls/src/main.rs
+++ rust_ls/src/main.rs
@@ -12,32 +12,30 @@ fn main() {
     }
 }
 
-fn parse_args(args:Vec<String>) -> Result<&'static Path, &'static str> {
+fn parse_args(args:Vec<String>) -> Result<String, &'static str> {
     match args.len(){
-        1 => Ok(Path::new("./")),
-        2 => {
-                if Path::new(&args[1]).exists(){
-                    Ok(Path::new(&args[1]))
-                }else{
-                    Err("Please pass a path for listing files.")
-                }
-            },
+        1 => Ok(String::from("./")),
         _ => {
-            Err("Please pass only one path string for listing files.")
-        }
+                if Path::new(&args[1]).exists(){            
+                    Ok(args[1].clone())
+                }else{
+                    Err("Please pass a path string for listing files.")
+                }
+            }
     }
 }
 
 #[test]
 fn test_arg_parse1(){
-    let input = vec!["","./"];
-    let result = parse_args(&input);
-    assert_eq!(result, "");
+    let input = vec!["".to_string(),"./".to_string()];
+    let result = parse_args(input);
+    assert_eq!(result.unwrap(), "./".to_string());
 }
 
 #[test]
+#[should_panic]
 fn test_arg_parse2(){
-    let input2 = vec!["","123"];
-    let result2 = parse_args(&input2);
-    assert_eq!(result2, "");   
+    let input2 = vec!["".to_string(),"12387".to_string()];
+    let result2 = parse_args(input2);
+    let x = result2.unwrap();   
 }
