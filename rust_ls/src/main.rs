93afb775a7f2d053ce1b4273ef6347f9f5a158be
Michael Droessler <droessmj@gmail.com>
2019-03-10 17:21:38 -0500

    sorts output, trims input dir off string

--- rust_ls/src/main.rs
+++ rust_ls/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::fs::DirEntry;
 use std::env;
 use std::path::Path;
 
@@ -6,10 +7,21 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let path = parse_args(args);
     let p = path.unwrap();
+    let i = p.len();
     let paths = fs::read_dir(p).unwrap();
-    for path in paths {
-        println!("{}", path.unwrap().path().display());
+    let mut output:String = String::new();
+
+    let mut paths_vec: Vec<DirEntry> = paths.map(Result::unwrap).collect();
+    paths_vec.sort_unstable_by(|a,b| a.path().cmp(&b.path()));
+
+    for path in paths_vec {
+        let s = path.path().display().to_string();
+        //substring to remove the path from front
+        let s_sub = &s[i..s.len()];
+        output.push_str(&format!("{}  ", s_sub));        
     }
+    
+    println!("{}",output);
 }
 
 fn parse_args(args:Vec<String>) -> Result<String, &'static str> {
@@ -37,5 +49,12 @@ fn test_arg_parse1(){
 fn test_arg_parse2(){
     let input2 = vec!["".to_string(),"12387".to_string()];
     let result2 = parse_args(input2);
-    let x = result2.unwrap();   
+    result2.unwrap();   
+}
+
+#[test]
+fn test_substring(){
+    let input = "123456";
+    let result = &input[2..6];
+    assert_eq!(result, "3456");
 }
