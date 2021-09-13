3f983bf916660ce9db7fa3b3f58114e1471bbdac
Michael Droessler <michael.droessler@lacework.net>
2021-09-13 10:32:47 -0500

    gitignore fix

--- day_dir/target/debug/day_dir.d
+++
@@ -1,1 +0,0 @@
-/Users/doc/Workspace/code/personal/rust/day_dir/target/debug/day_dir: /Users/doc/Workspace/code/personal/rust/day_dir/src/main.rs
--- day_dir/target/debug/deps/day_dir.d
+++
@@ -1,5 +0,0 @@
-/Users/doc/Workspace/code/personal/rust/day_dir/target/debug/deps/day_dir: src/main.rs
-
-/Users/doc/Workspace/code/personal/rust/day_dir/target/debug/deps/day_dir.d: src/main.rs
-
-src/main.rs:
